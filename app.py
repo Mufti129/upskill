e1b04141784cd7dd9354c8d6bfbed7bf121ed504
Mufti129 <ukhtytrader01@gmail.com>
2026-02-23 14:27:57 +0700

    app.py

--- app.py
+++ app.py
@@ -178,12 +178,16 @@ selected_city = st.sidebar.multiselect(
     df["city"].dropna().unique(),
     default=df["city"].dropna().unique()
 )
+completed_only = st.sidebar.checkbox("Completed Only", value=True)
 
 filtered_df = df[
     (df["year"] == selected_year) &
     (df["city"].isin(selected_city))
 ]
-
+if completed_only:
+    filtered_df = filtered_df[
+        filtered_df["Status"].str.lower() == "completed"
+    ]
 if filtered_df.empty:
     st.warning("Tidak ada data untuk filter yang dipilih.")
     st.stop()
